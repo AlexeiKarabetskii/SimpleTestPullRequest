ae0538400dfe1320ef2dcbb2740a8e5f32ada1d9
christinchick <alexey@skyward-crimea.ru>
2022-02-26 12:46:54 +0300

    new tests

---
+++ src/test/java/alexeyWhithCristinchick/SimpleTest.java
@@ -0,0 +1,17 @@
+package alexeyWhithCristinchick;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void test00() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(true);
+    }
+}
